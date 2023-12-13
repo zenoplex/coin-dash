7d5ca552655b96a9a401f2b230944a2f18f9716f
Gen Ichino <zenoplex@gmail.com>
2023-12-14 01:30:35 +0900

    feat: Add player movement functionality

--- player.gd
+++ player.gd
@@ -4,3 +4,7 @@ extends Area2D
 
 var velocity:Vector2 = Vector2.ZERO
 var screensize:Vector2 = Vector2(480, 720)
+
+func _process(delta: float) -> void:
+  velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+  position += velocity * speed * delta
