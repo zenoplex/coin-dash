eebed6ecf9781bd5221e25f4dc2d638a6383494c
Gen Ichino <zenoplex@gmail.com>
2023-12-18 17:44:55 +0900

    feat: Add coin spawning functionality

--- main.gd
+++ main.gd
@@ -14,6 +14,16 @@ func _ready() -> void:
 	level = 1
 	score = 0
 	time_left = play_time
+	screensize = get_viewport().get_visible_rect().size
 	$Player.start()
 	$Player.show()
 	$GameTimer.start()
+	spawn_coins()
+
+func spawn_coins() -> void:
+	for i in level + 4:
+		var node:Node = coin_scene.instantiate()
+		node.screensize = screensize
+		node.position = Vector2(randi_range(0, int(screensize.x)), randi_range(0, int(screensize.y)))
+		add_child(node);
+
