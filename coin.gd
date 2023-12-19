dfff99f40ffdec39b0503e635d10a5266883acce
Gen Ichino <zenoplex@gmail.com>
2023-12-19 11:16:35 +0900

    feat: Add pickup animation and disable collision during animation

--- coin.gd
+++ coin.gd
@@ -3,4 +3,11 @@ extends Area2D
 var screensize:Vector2 = Vector2.ZERO
 
 func pickup() -> void:
+	# Wait for the current frame to finish
+	$CollisionShape2D.set_deferred("disabled", true)
+	var tween:Tween = create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "scale", scale * 3, 0.3)
+	# modulate:a refers to the alpha value of the color
+	tween.tween_property(self, "modulate:a", 0.0, 0.3)
+	await tween.finished
 	queue_free()
